172f2ec3600994c4927913e25fecc2bab039deed
Unknown <jhample323@west-mec.org>
2018-09-05 18:16:46 -0700

    Trying to figure out the mess i've made
    I have screwed up something when uploading to the server while trying to finish this project

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 /*  Project 01_11_02
 
-    Author: 
-    Date:   
+    Author: Jaggar Hample
+    Date: 9/5/18  
 
     Filename: script.js
 */
 
 "use strict";
 
-// Global Variables
+// Global Variable
 var httpRequest = false;
 var entry = "^IXIC";
 
@@ -19,7 +19,25 @@ function getRequestObject() {
     } catch (requestError) {
         return false;
     }
-    alert(httpRequest);
     return httpRequest;
 }
-test
+
+// function to stop default submission
+function stopSubmission(evt) {
+    if (evt.preventDefault) {
+        evt.preventDefault();
+    } else {
+        evt.returnValue = false;
+    }
+}
+
+// set up temporary event handler
+var form = document.getElementsByTagName("form")[0];
+if (form.addEventListener) {
+    form.addEventListener("submit", stopSubmission, false);
+} else if (form.attachEvent) {
+    form.attachEvent("onsubmit", stopSubmission);
+    window.attachEvent("onload", getRequestObject);
+}
+
+form
